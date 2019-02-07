cf94a1b4b4c24039856ec0101994d42f0a9d5b84
Edward Liang <kentoruya@gmail.com>
2019-02-06 22:44:27 -0800

    Updated constructors and added a Utility

---
+++ SFML_tester/Utility.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef Utility_h
+#define Utility_h
+#include <iostream>
+using namespace std;
+
+class Utility {
+public:
+	void getLine(void);
+	bool str2bool(string);
+};
+
+#endif
---
+++ SFML_tester/scriptLine.cpp
@@ -0,0 +1,15 @@
+#include "scriptLine.h"
+#include "Utility.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+scriptLine::scriptLine()
+{
+	Utility function;
+	function.getLine();
+	
+	
+}
