a15dded2022699e342f9bb7b17ed107bd82eba6a
Edward Liang <kentoruya@gmail.com>
2019-02-07 03:46:02 -0800

    some other fixes

--- SFML_tester/Config.cpp
+++ SFML_tester/Config.cpp
@@ -53,6 +53,9 @@ void Config::parse(string configFile)
 	string tempStr;			// Temporary string used as we parse line
 	int tempInt;			// Temporary int used as we parse line
 
+
+	// I have yet to test if this is the right way to keep reading new line.
+	// The rename ss convention is TERRIBLE, will test tomorrow.
 	getline(myFileStream, line);
 	stringstream ss(line);
 	getline(ss, tempStr, '\t');
@@ -60,68 +63,68 @@ void Config::parse(string configFile)
 	masterVolume = stof(tempStr);
 	
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream tt(line);
+	getline(tt, tempStr, '\t');
+	getline(tt, tempStr);
 	bgmVolume = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream yy(line);
+	getline(yy, tempStr, '\t');
+	getline(yy, tempStr);
 	voiceVolume = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream zz(line);
+	getline(zz, tempStr, '\t');
+	getline(zz, tempStr);
 	sfxVolume = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream qq(line);
+	getline(qq, tempStr, '\t');
+	getline(qq, tempStr);
 	fullScreen = stoi(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream xx(line);
+	getline(xx, tempStr, '\t');
+	getline(xx, tempStr);
 	manualTextSpeed = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream cc(line);
+	getline(cc, tempStr, '\t');
+	getline(cc, tempStr);
 	autoTextSpeed = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream vv(line);
+	getline(vv, tempStr, '\t');
+	getline(vv, tempStr);
 	autoTextWaitTime = stof(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream bb(line);
+	getline(bb, tempStr, '\t');
+	getline(bb, tempStr);
 	textFade = util.str2bool(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream nn(line);
+	getline(nn, tempStr, '\t');
+	getline(nn, tempStr);
 	skipUnreadText = util.str2bool(tempStr);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, fontFileName);
+	stringstream mm(line);
+	getline(mm, tempStr, '\t');
+	getline(mm, fontFileName);
 
 	getline(myFileStream, line);
-	stringstream ss(line);
-	getline(ss, tempStr, '\t');
-	getline(ss, tempStr);
+	stringstream ll(line);
+	getline(ll, tempStr, '\t');
+	getline(ll, tempStr);
 	textWindowTransparency = stof(tempStr);
 }
 
--- SFML_tester/charPic.h
+++ SFML_tester/charPic.h
@@ -6,4 +6,6 @@ class CharPic {
 public:
 	int xPos, yPos;
 	string picName;
+
+	CharPic();
 };
